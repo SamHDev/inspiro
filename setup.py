6df11bf576b16d06f0005c228f4ecfde7a1ef387
Sam Huddart <sam02h.huddart@gmail.com>
2021-01-28 20:17:41 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="insprio",
+    name="inspiro",
     version="0.0.1",
     author="SamHDev",
     author_email="sam02h.huddart@gmail.com",
